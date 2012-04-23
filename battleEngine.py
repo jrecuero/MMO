9b409202acf6c0fb3f0db86106fe05289e284066
Jose Carlos Recuero <crecuero@crecuero-linux.(none)>
2012-04-23 12:11:24 -0700

    Add life

--- battleEngine.py
+++ battleEngine.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 
 class Weapon(object):
+    """ """
     
     def __init__(self):
         pass
     
     
 class Magic(object):
+    """ """
     
     def __init__(self):
         pass
     
 
 class Actor(object):
+    """ """
 
     def __init__(self, name):
         self.name       = name
         self.weakness   = None
         self.inmune     = None
         self.robustness = None
+        self.life       = 0
         pass
 
+    def isAlive(self):
+        return self.life > 0
+    
 
 class BattleEngine(object):
+    """ """
     
     def __init__(self):
         pass
