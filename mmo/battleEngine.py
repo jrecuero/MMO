9a82a97ba20fb955b9c5e326e72e06295a3047e9
Jose Carlos Recuero <crecuero@crecuero-linux.(none)>
2012-04-23 15:36:38 -0700

    Add unit test code

--- mmo/battleEngine.py
+++ mmo/battleEngine.py
@@ -2,7 +2,9 @@
 
 class Weapon(object):
     """ """
-    
+
+    Type = { 'SLASH' : 0, 'BLUNT' : 1 }
+
     def __init__(self):
         pass
     
@@ -19,19 +21,25 @@ class Actor(object):
 
     def __init__(self, name):
         self.name       = name
-        self.weakness   = None
-        self.inmune     = None
-        self.robustness = None
+        self.weakness   = []
+        self.inmune     = []
+        self.robustness = []
         self.life       = 0
         pass
 
     def isAlive(self):
         return self.life > 0
     
+    def setWeakness(self, weak):
+        self.weakness.append(weak)
 
 class BattleEngine(object):
     """ """
     
     def __init__(self):
         pass
-    
+  
+if __name__ == '__main__':
+    actor = Actor('x')
+    actor.setWeakness(Weapon.Type['BLUNT'])
+    print actor.weakness
--- test/testBattleEngine.py
+++ test/testBattleEngine.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
+import mmo.battleEngine as be
 import unittest
+
+class TestBattleEngine(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_sample(self):
+        print 'Battle Engine Unit Test'
+
+if __name__ == '__main__':
+    unittest.main()
